4bed9ab2de2552a35adb537cc190b45a3e5df3dc
cgezer <celalgezer@gmail.com>
2023-06-11 03:49:01 +0300

    autocomplete

--- blog.js
+++ blog.js
@@ -9,3 +9,10 @@ VERSION:v.1
 darkMode=()=>{
     let darkButton=document.body.classList.toggle("dark_mode_classlist")
 }
+$(function(){
+const searchData=["Adıyaman","Bandırma","Cize","Diyarbakır","Erdemli","Mardin"];
+$("#tags").autocomplete({
+    source:searchData
+});
+}
+);
