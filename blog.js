4d0f7175953278d574f56e42cc174448ce6c121b
cgezer <celalgezer@gmail.com>
2023-06-11 04:30:43 +0300

    back top

--- blog.js
+++ blog.js
@@ -6,9 +6,11 @@ AUTHOR:By Celal GEZER
 DATE:09.05.2023
 VERSION:v.1
 */
+/* DARK MODE*/
 darkMode=()=>{
     let darkButton=document.body.classList.toggle("dark_mode_classlist")
 }
+/* SEARCH*/
 $(function(){
 const searchData=["Adıyaman","Bandırma","Cize","Diyarbakır","Erdemli","Mardin"];
 $("#tags").autocomplete({
@@ -16,3 +18,18 @@ $("#tags").autocomplete({
 });
 }
 );
+
+/* BACK TOP */
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $("#back_top").fadeIn('slow');
+        }else{
+            $("#back_top").fadeOut('slow');
+        }
+    });
+    $("#back_top").click(function(){
+        $("html, body").animate({scrollTop:0},90,'easeInOutExpo');
+        return false;
+    })
+});
